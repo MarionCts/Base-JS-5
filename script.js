7a603a30866eb2e61515db92ebd5842b8f0a065e
Marion Cts <104251209+MarionCts@users.noreply.github.com>
2025-05-20 09:55:38 +0200

    Adding second solution

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ for (let nombre = 0; nombre <= 100 ; nombre++) {
 console.log(`${multiples}`);
 
 // Exo 2
+
+const notes =  [15, -3, 12, 0, 19, 22, 8];
+let valides = [];
+let i = 0;
+
+while(i < notes.length) {
+    let note = notes[i];
+    if (note >= 0 && note <= 20) {
+            valides.push(note);
+        }
+        i++;
+        
+}
+
+console.log(`Les notes valides sont ${valides}`);
+
+console.log(`Le nombre de notes invalides est de ${notes.length - valides.length}.`);
